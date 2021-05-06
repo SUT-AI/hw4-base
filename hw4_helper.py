2042abc136f7f88debf4c25d59b2deb37fd20545
ahmadsalimi <ahsa9978@gmail.com>
2021-05-06 23:19:58 +0430

    Update docs

--- hw4_helper.py
+++ hw4_helper.py
@@ -1,12 +1,26 @@
+r""" A helper module for Practical HW of neural networks, AI course, Sharif University of Technology
+
+Usage:
+>>> import hw4_helper
+>>> x_train, y_train = hw4_helper.get_train_data()
+>>> x_test = hw4_helper.get_test_data()
+>>> prediction = np.zeros(100)
+>>> hw4_helper.export_prediction(prediction)
+
+Author:
+    Ahmad Salimi - https://github.com/ahmadsalimi
+"""
+
 from typing import Tuple, List, Iterable
 import os
 from enum import Enum
+
 import requests
 import numpy as np
 from tqdm import tqdm
 
 
-class FileInfo:
+class _FileInfo:
 
     def __init__(self, name: str, parts: List[str] = ['']):
         self.name = name
@@ -18,10 +32,10 @@ CACHE_ROOT = 'data_cache'
 BASE_URL = 'https://sut-ai.github.io/hw4-base/assets/'
 
 
-class DataSpecification(Enum):
-    TrainX = FileInfo('x_train.npy', ['.aa', '.ab', '.ac'])
-    TrainY = FileInfo('y_train.npy')
-    TestX = FileInfo('x_test.npy')
+class _DataSpecification(Enum):
+    TrainX = _FileInfo('x_train.npy', ['.aa', '.ab', '.ac'])
+    TrainY = _FileInfo('y_train.npy')
+    TestX = _FileInfo('x_test.npy')
 
 
 def _download_file(responses: Iterable[requests.Response], size: int, filename: str):
@@ -39,18 +53,21 @@ def _get_response(filename: str, part: str) -> requests.Response:
     return requests.get(f'{BASE_URL}{filename}{part}', stream=True)
 
 
-def _get_size(file_info: FileInfo) -> int:
-    responses = map(lambda part: _get_response(file_info.name, part), file_info.parts)
+def _get_size(file_info: _FileInfo) -> int:
+    responses = map(lambda part: _get_response(
+        file_info.name, part), file_info.parts)
     return sum(map(lambda res: int(res.headers['Content-Length']), responses))
 
 
-def _get_data_by_file_info(file_info: FileInfo) -> np.ndarray:
-    responses = map(lambda part: _get_response(file_info.name, part), file_info.parts)
+def _get_data_by_file_info(file_info: _FileInfo) -> np.ndarray:
+    responses = map(lambda part: _get_response(
+        file_info.name, part), file_info.parts)
     os.makedirs(CACHE_ROOT, exist_ok=True)
-    _download_file(responses, _get_size(file_info), os.path.join(CACHE_ROOT, file_info.name))
+    _download_file(responses, _get_size(file_info),
+                   os.path.join(CACHE_ROOT, file_info.name))
 
 
-def _get_data(file_info: FileInfo) -> np.ndarray:
+def _get_data(file_info: _FileInfo) -> np.ndarray:
     file_path = os.path.join(CACHE_ROOT, file_info.name)
     if not os.path.exists(file_path):
         _get_data_by_file_info(file_info)
@@ -59,16 +76,35 @@ def _get_data(file_info: FileInfo) -> np.ndarray:
 
 
 def get_train_data() -> Tuple[np.ndarray, np.ndarray]:
-    x = _get_data(DataSpecification.TrainX.value)
-    y = _get_data(DataSpecification.TrainY.value)
+    r"""
+    Downloads the train dataset (if needed) and returns
+
+            Returns:
+                    x (np.ndarray): images of the dataset.  shape: (N, 28, 28)
+                    y (np.ndarray): labels of the images.   shape: (N,)
+    """
+    x = _get_data(_DataSpecification.TrainX.value)
+    y = _get_data(_DataSpecification.TrainY.value)
     return x, y
 
 
 def get_test_data() -> np.ndarray:
-    return _get_data(DataSpecification.TestX.value)
+    r"""
+    Downloads the test dataset (if needed) and returns
+
+            Returns:
+                    x (np.ndarray): images of the dataset.  shape: (N, 28, 28)
+    """
+    return _get_data(_DataSpecification.TestX.value)
 
 
 def export_prediction(prediction: np.ndarray):
+    r"""
+    Exports the given prediction array to `prediction.npy`
+
+            Parameters:
+                    prediction (np.ndarray): The prediction array. (one dimensional)
+    """
     assert isinstance(prediction, np.ndarray),\
         f"Expected prediction to be a numpy.ndarray, got {type(prediction)}"
     assert prediction.ndim == 1,\
